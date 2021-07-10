e77108f203f8fcdcfdb864c6815806678c44d3a6
angelopoole <angelo.poole@outlook.com>
2021-07-10 18:17:40 -0400

    added async-handler to userController.ts

--- Backend/controllers/userController.ts
+++ Backend/controllers/userController.ts
@@ -1,23 +1,23 @@
 import Express, { Request, Response } from 'express';
-import expressAsyncHandler from 'express-async-handler';
+import asyncHandler from 'express-async-handler';
 
 //  @desc
 //  @route
 //  @access
-const registerUser = async (req: Request, res: Response) => {
+const registerUser = asyncHandler((req: Request, res: Response) => {
   try {
     res.send('registerUser');
   } catch (err) {
     console.log(err);
   }
-};
+});
 
-const getTest = async (req: Request, res: Response) => {
+const getTest = asyncHandler((req: Request, res: Response) => {
   try {
     res.send('get that test bro');
   } catch (err) {
     console.error(err);
   }
-};
+});
 
 export { registerUser, getTest };
