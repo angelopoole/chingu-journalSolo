ed50101871bc4365ed8f42fcaaf3701e7ca3bbbc
angelopoole <angelo.poole@outlook.com>
2021-07-10 19:03:49 -0400

    alias imports not supported yet in create-react-app ? might have to eject :)

--- Client/src/components/App.tsx
+++ Client/src/components/App.tsx
@@ -1,5 +1,6 @@
 import { promises } from 'fs';
 import React, { useState, useEffect } from 'react';
+import { GlobalStyles } from '@styles';
 
 // here would be pages within a router, initial fetch calls, state management and context api useage.
 
@@ -7,7 +8,7 @@ import React, { useState, useEffect } from 'react';
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
-
+  console.log(promises);
   useEffect(() => {
     if (isLoading) {
     }
