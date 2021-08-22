a1ab951dfc152630d9da86519aeb15a6de02f0e9
angelopoole <angelo.m.poole@gmail.com>
2021-08-22 16:15:02 -0400

    change hompage file name

--- Client/src/components/App.tsx
+++ Client/src/components/App.tsx
@@ -6,7 +6,7 @@ import { ProvideAuth } from '../hooks/use-auth';
 import Layout from './Layout';
 import AuthPage from '../pages/AuthPage';
 import NotFoundPage from '../pages/NotFoundPage';
-import HomePage from '../pages/Homepage';
+import HomePage from '../pages/HomePage';
 
 function App() {
   return (
