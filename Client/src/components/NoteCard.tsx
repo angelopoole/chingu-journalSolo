216a493ed1ed42442f91196e05f98d1dce082904
angelopoole <angelo.m.poole@gmail.com>
2021-08-31 16:41:02 -0400

    begin card refactor

--- Client/src/components/NoteCard.tsx
+++ Client/src/components/NoteCard.tsx
@@ -2,74 +2,43 @@ import React from 'react';
 import styled from 'styled-components';
 import { Note } from '../interfaces/NoteTypes';
 
-const StyledOuterCard = styled.div`
-  display: flex;
-  flex-flow: column;
-  justify-content: space-between;
-  margin: 0.4rem;
-
-  border: var(--dark-accent) solid;
-  background-color: var(--light-shades);
-  border-radius: 1rem;
-`;
-
-const StyledContent = styled.div`
-  height: 80%;
-  border-bottom: 3px var(--dark-accent) solid;
-  .title {
-    background-color: var(--light-accent);
-    margin-top: 0;
-    margin-bottom: 0.1rem;
-    border-top-left-radius: 0.8rem;
-    border-top-right-radius: 0.8rem;
-    text-align: center;
-  }
-
-  p {
-    height: 80%;
-    margin: 0.3rem;
-  }
-`;
-
 // TODO, fix how cards are created, instead go with a 4 area grid so that the outer card controlls the coloring on hover.
-
-const StyledButtonContainer = styled.div`
-  justify-self: flex-end;
-  display: flex;
-  justify-content: space-around;
-  align-content: flex-end;
-  height: 20%;
-
-  button {
-    all: unset;
-    width: 100%;
-    height: 100%;
-    text-align: center;
-    cursor: pointer;
-  }
-  button:nth-child(1) {
-    margin-right: 0;
-    border-right: 0.3rem var(--dark-accent) solid;
-    border-bottom-left-radius: 0.8rem;
-    background-color: var(--light-accent);
-  }
-  button:nth-child(2) {
-    margin-left: 0;
-    border-left: 0.3rem var(--dark-accent) solid;
-    border-bottom-right-radius: 0.8rem;
-    background-color: red;
-  }
-
-  button:hover {
-    background-color: yellow;
-  }
-`;
-
 // TODO, onclick notes should open into modal.
 // TODO, edit and delete should be done inside modal
 // TODO, edits done inside of the modal should be sent to the backed after a typing delay, 5000ms
 // TODO, remove edit and delete buttons from card.
 // TODO, stop card overflow, this causes responsiveness breaks.
+
+const StyledCardContainer = styled.div`
+  margin: 10px;
+  background-color: var(--light-shades);
+`;
+
+const StyledCard = styled.div`
+  height: 100%;
+  border: 2px solid var(--dark-accent);
+  border-radius: 0.5em;
+`;
+
+const StyledContentContainer = styled.div`
+  padding: 5px;
+  display: flex;
+  height: 85%;
+`;
+
+const StyledContent = styled.div`
+  min-height: 60px;
+`;
+
+const StyledTitle = styled.div``;
+
+const StyledBodyContainer = styled.div``;
+
+const StyledBody = styled.div``;
+
+const StyledflexButtons = styled.div`
+  display: flex;
+`;
 
 const NoteCard = ({
   note,
@@ -90,17 +59,49 @@ const NoteCard = ({
   };
 
   return (
-    <StyledOuterCard>
-      <StyledContent>
-        <h3 className='title'>{title}</h3>
-        <p className='body'>{body}</p>
-      </StyledContent>
-      <StyledButtonContainer className='button-container'>
-        <button onClick={() => handleModalAndSetEditNote(note)}>edit</button>
-        <button onClick={() => deleteNote(_id)}>delete</button>
-      </StyledButtonContainer>
-    </StyledOuterCard>
+    <StyledCardContainer id='1 CardContainer'>
+      <StyledCard id=' 2 Card'>
+        <StyledContentContainer id=' 3 Info and flexdecision container'>
+          <StyledContent id='4 titleBodyContent'>
+            <StyledTitle id='title'>{title}</StyledTitle>
+            <StyledBodyContainer id='bodyContiner'>
+              <StyledBody id='body'>{body}</StyledBody>
+            </StyledBodyContainer>
+          </StyledContent>
+        </StyledContentContainer>
+        <StyledflexButtons id=' 3 interactionIcons -Flex'>
+          <button onClick={() => handleModalAndSetEditNote(note)}>edit</button>
+          <button onClick={() => deleteNote(_id)}>delete</button>
+        </StyledflexButtons>
+      </StyledCard>
+    </StyledCardContainer>
   );
+
+  // return (
+  //   <StyledOuterCard>
+  //     <StyledContent>
+  //       <h3 className='title'>{title}</h3>
+  //       <p className='body'>{body}</p>
+  //     </StyledContent>
+  //     <StyledButtonContainer className='button-container'>
+  //       <button onClick={() => handleModalAndSetEditNote(note)}>edit</button>
+  //       <button onClick={() => deleteNote(_id)}>delete</button>
+  //     </StyledButtonContainer>
+  //   </StyledOuterCard>
+  // );
 };
 
 export default NoteCard;
+
+{
+  /* <StyledOuterCard>
+<StyledContent>
+  <h3 className='title'>{title}</h3>
+  <p className='body'>{body}</p>
+</StyledContent>
+<StyledButtonContainer className='button-container'>
+  <button onClick={() => handleModalAndSetEditNote(note)}>edit</button>
+  <button onClick={() => deleteNote(_id)}>delete</button>
+</StyledButtonContainer>
+</StyledOuterCard> */
+}
