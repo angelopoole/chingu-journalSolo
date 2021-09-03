d9717fb2ae9fd7cd1170a924eb7243fa7c7200be
angelopoole <angelo.m.poole@gmail.com>
2021-09-02 21:29:41 -0400

    add landingpage, update routes to landing page and homepage, update redirects, update styles

--- Client/src/components/sections/Footer.tsx
+++ Client/src/components/sections/Footer.tsx
@@ -3,6 +3,11 @@ import React from 'react';
 import styled from 'styled-components';
 
 const StyledFooter = styled.footer`
+  position: relative;
+  padding: 10px 10px 0px 10px;
+  bottom: 0;
+  width: 100%;
+
   min-height: 3em;
   color: #fff;
   background-color: var(--dark-shades);
